a5a214161633a606827bd2c74128e9b66e6be864
Alejandro Perez <aleperez92@gmail.com>
2014-04-23 23:11:59 +0100

    Created files and basic structure

---
+++ code.js
@@ -0,0 +1,8 @@
+var submitButton = document.getElementsByName("submit")[0],
+    checkbox = document.getElementsByName("checkbox")[0],
+    textArea = document.getElementsByName("textarea").value;
+
+// Attach events to submit button
+submitButton.addEventListener("click", function() {
+    alert();
+});
