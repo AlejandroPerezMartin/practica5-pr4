d794807f0d0359b8dd7a258353d8806fbfc82502
Alejandro Perez <aleperez92@gmail.com>
2014-04-29 21:39:44 +0100

    More advances

--- code.js
+++ code.js
@@ -3,45 +3,69 @@
 var submitButton = document.getElementById("submit");
 
 // Attach events to submit button
-submitButton.addEventListener("click", function() {
-    
+submitButton.addEventListener("click", function(e) {
+
+    // Prevent button default action
+    e.preventDefault();
+
     // Variable declaration
-    var checkbox = document.getElementById("checkbox"),
-        checkbox2 = document.getElementById("checkbox2"),
-        textAreaValue = document.getElementById("textarea").value,
-        textAreaLength = textAreaValue.length,
+    var isFirstCheckboxActive = document.getElementById("checkbox").checked,
+        isSecondCheckboxActive = document.getElementById("checkbox2").checked,
+        resultsDiv = document.getElementById("results"),
+        textAreaString = document.getElementById("textarea").value,
+        textAreaLength = textAreaString.length,
+        numberOfChars = 0,
         numberOfLines,
         numberOfWords,
-        separatedWords,
         averageWordLength,
-        numberOfChars = 0,
+        listOfWords,
+        listOfWordsAndFreq = [], // Array to store [{frequency: int, word: "str"}, ...]
+        frequency = 1,
         i = 0;
 
     // Get number of lines
-    numberOfLines = textAreaValue.split("\n").length;
+    numberOfLines = textAreaString.split("\n").length;
 
     // Replace non alphabetic characters with whitespace
     for (i; i < textAreaLength; i++) {
-        if (textAreaValue[i].toUpperCase() === textAreaValue[i].toLowerCase()) {
-            textAreaValue = textAreaValue.replace(textAreaValue[i], " ");
+        if (textAreaString[i].toUpperCase() === textAreaString[i].toLowerCase()) {
+            textAreaString = textAreaString.replace(textAreaString[i], " ");
         }
     }
 
     // Split string by whitespace to get words and store them in an array
-    separatedWords = textAreaValue.replace(/\s+/g, ' ').replace(/^\s+|\s+$/, '').trim().split(' ');
+    listOfWords = textAreaString.replace(/\s+/g, ' ').replace(/^\s+|\s+$/, '').trim().split(' ').sort();
 
     // Get number of words
-    numberOfWords = separatedWords.length;
+    numberOfWords = listOfWords.length;
 
-    // Get total number of characters
-    averageWordLength = separatedWords.forEach(function(word) {
-        numberOfChars += word.length;
-    });
+    // Count number of occurrences and store in key:value array listOfWordsAndFreq
+    for (i = 0; i < numberOfWords; i++) {
+        if ((i + 1) < numberOfWords && listOfWords[i] === listOfWords[i + 1]) {
+            frequency++;
+        }
+        else {
+            listOfWordsAndFreq.push({
+                "freq": frequency,
+                "word": listOfWords[i]
+            });
+            frequency = 1; // reset variable for next word
+        }
+
+        // Get total number of characters
+        numberOfChars += listOfWords[i].length;
+    }
+
+    averageWordLength = Math.floor(numberOfChars / numberOfWords);
 
     // Show results in console
-    console.log("Words: " + separatedWords);
+    console.log("Words: " + listOfWords);
     console.log("Number of words: " + numberOfWords);
     console.log("Number of lines: " + numberOfLines);
-    console.log("Average word length: " + Math.floor(numberOfChars / numberOfWords));
+    console.log("Average word length: " + averageWordLength);
+    for (i = 0; i < listOfWordsAndFreq.length; i++) {
+        console.log(listOfWordsAndFreq[i].freq + " occurrences of '" + listOfWordsAndFreq[i].word + "'");
+    };
+    console.log("---------------------");
 
 });
