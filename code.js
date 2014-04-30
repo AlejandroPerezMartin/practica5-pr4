81905509a5140db29134d1d0a2f6a699042dd23e
Alejandro Perez <aleperez92@gmail.com>
2014-04-30 23:57:08 +0100

    Removed console output

--- code.js
+++ code.js
@@ -11,16 +11,16 @@ submitButton.addEventListener("click", function(e) {
     // Variable declaration
     var isFirstCheckboxActive = document.getElementById("checkbox").checked,
         isSecondCheckboxActive = document.getElementById("checkbox2").checked,
-        resultsDiv = document.getElementById("results"),
         textAreaString = document.getElementById("textarea").value,
         textAreaLength = textAreaString.length,
-        numberOfChars = 0,
-        numberOfLines,
-        numberOfWords,
-        averageWordLength,
-        listOfWords,
         listOfWordsAndFreq = [], // Array to store [{frequency: int, word: "str"}, ...]
+        numberOfChars = 0,
+        outputItems = [],
         frequency = 1,
+        numberOfWords,
+        numberOfLines,
+        listOfWords,
+        outputHtml,
         i = 0;
 
     // Get number of lines
@@ -33,8 +33,8 @@ submitButton.addEventListener("click", function(e) {
         }
     }
 
-    // Split string by whitespace to get words and store them in an array
-    listOfWords = textAreaString.replace(/\s+/g, ' ').replace(/^\s+|\s+$/, '').trim().split(' ').sort();
+    // Remove duplicated whitespaces and split string by whitespace to get words and store them in an array
+    listOfWords = textAreaString.replace(/\s+/g, " ").replace(/^\s+|\s+$/, "").trim().split(" ").sort();
 
     // Get number of words
     numberOfWords = listOfWords.length;
@@ -42,30 +42,40 @@ submitButton.addEventListener("click", function(e) {
     // Count number of occurrences and store in key:value array listOfWordsAndFreq
     for (i = 0; i < numberOfWords; i++) {
         if ((i + 1) < numberOfWords && listOfWords[i] === listOfWords[i + 1]) {
+            // If current word is equal to previous one, increment number of occurrences by 1
             frequency++;
         }
         else {
+            // If current word matches its previous one, current word and its number of occurrences is stored in the array
             listOfWordsAndFreq.push({
                 "freq": frequency,
                 "word": listOfWords[i]
             });
-            frequency = 1; // reset variable for next word
+            frequency = 1; // reset occurrences for next word
         }
 
         // Get total number of characters
         numberOfChars += listOfWords[i].length;
     }
 
-    averageWordLength = Math.floor(numberOfChars / numberOfWords);
+    // Show results only if second checkbox is checked
+    if (isSecondCheckboxActive) {
+        outputItems.push("<li>Average word length: <span>" + Math.floor(numberOfChars / numberOfWords) + "</span></li>");
+        outputItems.push("<li>Number of words: <span>" + numberOfWords + "</span></li>");
+        outputItems.push("<li>Number of lines: <span>" + numberOfLines + "</span></li>");
+    }
 
-    // Show results in console
-    console.log("Words: " + listOfWords);
-    console.log("Number of words: " + numberOfWords);
-    console.log("Number of lines: " + numberOfLines);
-    console.log("Average word length: " + averageWordLength);
-    for (i = 0; i < listOfWordsAndFreq.length; i++) {
-        console.log(listOfWordsAndFreq[i].freq + " occurrences of '" + listOfWordsAndFreq[i].word + "'");
-    };
-    console.log("---------------------");
+    // Generate output
+    outputHtml = "<ul>";
+
+    i = 0;
+    for (i; i < outputItems.length; i++) {
+        outputHtml += outputItems[i];
+    }
+
+    outputHtml += "</ul>";
+
+    // Write output in results div
+    document.getElementById("results").innerHTML = outputHtml;
 
 });
